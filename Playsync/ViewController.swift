cac82355c4b31f1b4ca6525793daaaa2a6fb6407
Davis Hoang <viet@viets-imac.local>
2018-06-28 17:14:21 +0700

    Initial Commit

---
+++ Playsync/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Playsync
+//
+//  Created by Viet on 28/6/18.
+//  Copyright © 2018 Viet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
