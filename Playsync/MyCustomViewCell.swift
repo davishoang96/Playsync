e9d92095feda8535e574d388d1ebf1c2800fe6bc
Davis Hoang <davishoang96@gmail.com>
2018-06-28 17:44:39 +0700

    Initialize repo

---
+++ Playsync/MyCustomViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MyCustomViewCell.swift
+//  Playsync
+//
+//  Created by Viet on 28/6/18.
+//  Copyright © 2018 Viet. All rights reserved.
+//
+
+import Cocoa
+
+class MyCustomViewCell: NSTableCellView {
+
+    var onClickCheckBox: ((_ sender: NSButton) -> ())?
+    
+    @IBOutlet weak var CheckBox: NSButton!
+    @IBAction func onClickCheckBox(_ sender: NSButton) {
+        onClickCheckBox?(sender)
+    }
+    
+    
+}
--- Playsync/ViewController.swift
+++ Playsync/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import Cocoa
+import iTunesLibrary
+
+let library = try! ITLibrary.init(apiVersion: "1.1")
+let playlist = library.allPlaylists
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +25,35 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+}
 
-
+extension ViewController: NSTableViewDelegate, NSTableViewDataSource{
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return playlist.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if tableColumn?.identifier == "CheckColumn"{
+            if let cell: MyCustomViewCell = tableView.make(withIdentifier: "CheckColumn", owner: self) as? MyCustomViewCell
+            {
+                cell.CheckBox.setNextState()
+                cell.CheckBox.title = playlist[row].name
+                cell.onClickCheckBox = {sender in
+                    
+                    // Get all song in playlist
+                    
+                    for song in playlist[row].items{
+                        print(song.location?.lastPathComponent)
+                    }
+                    
+                    
+                    
+                    
+                }
+                return cell
+            }
+        }
+        return nil
+    }
 }
 
